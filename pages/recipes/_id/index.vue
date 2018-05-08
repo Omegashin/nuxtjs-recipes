f5bbd09980e4ecaef2cd00951f6e403faabd856a
bdcorps <bdcorps@gmail.com>
2018-05-08 19:51:38 -0400

    finished

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,55 @@
+<template>
+<section class="single-recipe">
+    <h1>{{recipe.title}}</h1>
+    <div>
+        <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title">
+    </div>
+    <p>{{recipe.descriptionText}}</p>
+</section>
+</template>
+
+<script scoped>
+export default {
+  asyncData (context) {
+    return new Promise ((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          recipe: [
+            {
+              id: "1",
+              title: "Yummy Chicken Burger",
+              previewText: "A yummy burger made with real chicken!",
+              descriptionText: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto necessitatibus odit, modi laboriosam quia tempora ut consequatur molestiae deleniti, nihil possimus facilis nostrum fugiat perferendis nesciunt autem dignissimos rem? Ipsa.",
+              thumbnail: "http://www.manathota.in/mandapeta/wp-content/uploads/2017/11/Spicy-Chipotle-Chicken-Burger-3.jpg"
+            },
+            {
+              id: "2",
+              title: "Rigatoni Pasta",
+              previewText: "Delicious pasta with cherry tomato sauce!",
+              descriptionText: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto necessitatibus odit, modi laboriosam quia tempora ut consequatur molestiae deleniti, nihil possimus facilis nostrum fugiat perferendis nesciunt autem dignissimos rem? Ipsa.",
+              thumbnail: "https://www.seriouseats.com/recipes/images/2016/08/20160827-cherry-tomato-pasta-13-1500x1125.jpg"
+            }
+          ].find(el => el.id === context.params.id)
+        })
+      }, 100)
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+.single-recipe {
+    flex-flow: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 30px;
+}
+
+.recipe-image {
+    width: 100%;
+}
+
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,54 @@
+<template>
+<section class="recipes">
+  <Recipe
+  v-for="recipe in recipes"
+  :key="recipe.id"
+  :thumbnail="recipe.thumbnail"
+  :title="recipe.title"
+  :id="recipe.id"
+  :previewText="recipe.previewText"
+  />
+</section>
+</template>
+
+<script>
+import Recipe from '@/components/Recipe'
+export default {
+  components: {
+    Recipe
+  },
+  asyncData () {
+    return new Promise ((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          recipes: [
+            {
+              id: "1",
+              title: "Yummy Chicken Burger",
+              previewText: "A yummy burger made with real chicken!",
+              thumbnail: "http://www.manathota.in/mandapeta/wp-content/uploads/2017/11/Spicy-Chipotle-Chicken-Burger-3.jpg"
+            },
+            {
+              id: "2",
+              title: "Rigatoni Pasta",
+              previewText: "Delicious pasta with cherry tomato sauce!",
+              thumbnail: "https://www.seriouseats.com/recipes/images/2016/08/20160827-cherry-tomato-pasta-13-1500x1125.jpg"
+            }
+          ]
+        })
+      }, 100)
+    })
+  }
+}
+</script>
+
+
+<style scoped>
+.recipes {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-content: center;
+  margin: 8px;
+}
+</style>
